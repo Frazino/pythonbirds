f8d00b887966724890d3f34ec23ab675c0756552
roger <rogerevandro81@gmail.com>
2023-01-02 17:28:02 -0300

    Criado atributos de instância

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=33):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá! {id(self)}'
 
 if __name__== '__main__':
-    p= Pessoa()
+    p= Pessoa('Luciano')
+    print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Roger'
+    print(p.nome)
+    print(p.idade)
